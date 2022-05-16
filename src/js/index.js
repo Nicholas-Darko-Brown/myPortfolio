db4bfe0c7a91c6f2f72448ae9189678192b01a05
Nicholas-Darko-Brown <nicholas.brown@amalitech.org>
2022-05-16 20:21:55 +0000

    Added gsap styling

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ const hamburger = document.getElementById("hamburger");
 const navList = document.getElementById("nav-list");
 
 hamburger.addEventListener("click", () => {
+  navList.style.animation = "5s ease-in";
   navList.classList.toggle("show");
 });
 
@@ -30,3 +31,12 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+// GSAP
+const timeline = gsap.timeline({ defaults: { duration: 1 } })
+timeline.from('header', { y: '-100%', ease: 'bounce' })
+        .from('li', { opacity: 0, stagger: .8 }, 1)
+        .from('main', { x: '-100vw' }, 1)
+        .from('article', { x: '100vw', ease: 'elastic'})
+        .from('.skill-box', { opacity: 0, stagger: 1 })
+        .from('.project', { opacity: 0, stagger: 1 })
